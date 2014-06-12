470a38f4de7234dd3ea1a50ec9f746b4ee68d814
Vincent Pretre <v.pretre@smartesting.com>
2014-06-12 16:29:23 +0200

    Fix travis script for coverage + ignore node and report directories

--- travis.sh
+++ travis.sh
@@ -2,6 +2,5 @@
 npm install
 npm install -g codeclimate-test-reporter
 
-CODECLIMATE_REPO_TOKEN=0e70ba0ce60de05fcb22e6a0466210b5aa755dbbeda1b742039cfcdf4f5bf925
 npm test
-codeclimate < report/lcov.info
+CODECLIMATE_REPO_TOKEN=0e70ba0ce60de05fcb22e6a0466210b5aa755dbbeda1b742039cfcdf4f5bf925 codeclimate < report/lcov.info
