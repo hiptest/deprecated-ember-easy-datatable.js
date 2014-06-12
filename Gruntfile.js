e68403bd769ee51b30134833317ac5616ca2e297
Vincent Pretre <v.pretre@smartesting.com>
2014-06-12 15:27:18 +0200

    Travis integration

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,16 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   grunt.initConfig({
     qunit: {
-      all: ['tests/index.html']
+      files: ['tests/index.html']
+    },
+    jshint: {
+      all: ['src/*.js']
     }
   });
+
+  grunt.registerTask('default', 'jshint');
+  grunt.registerTask('travis', ['jshint',  'qunit']);
 }
